eb03a9b18343b9e8bef2960978333dbef08ea678
Robert biernat <soulsea.demoncore@gmail.com>
2022-09-27 05:11:31 +0200

    Axios + json server working

--- src/views/PlanetsForm.vue
+++ src/views/PlanetsForm.vue
@@ -1,10 +1,12 @@
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       name: '',
-      habitability: 50,
-      oxygenPercent: 50,
+      habitability: true,
+      oxygen_percent: 50,
       type: 1,
       allTypes: [
       { text: 'Volcanic', value: 1 },
@@ -16,57 +18,54 @@ export default {
       { text: 'Exotic', value: 7 },
       { text: 'Wet', value: 8 },
       ],
-      animals: '',
-      allAnimals: [
-        { text: 'Mammals', value: 1 },
-        { text: 'Birds', value: 2 },
-        { text: 'Fish', value: 3 },
-        { text: 'Reptiles', value: 4 },
-        { text: 'Amphibians', value: 5 },
-        { text: 'Arthropods', value: 6 }
-      ],
-      plants: '',
-      allPlants: [
-        { text: 'Alpines', value: 1 },
-        { text: 'Annuals', value: 2 },
-        { text: 'Aquatic plants', value: 3 },
-        { text: 'Bulbs', value: 4 },
-        { text: 'Cacti', value: 5 },
-        { text: 'Carnivorous plants', value: 6 },
-        { text: 'Climbers', value: 7 },
-        { text: 'Ferns', value: 8 },
-      ],
+      discovery_date: new Date().toISOString().split('T')[0],
+      distance: 1000,
+      form_alert: '',
     }
-  }
-}
+  },
+  methods: {
+    async addItem() {
+      this.form_alert = '...';
+      const res = await axios.post(`http://localhost:3000/planets`, {
+        name: this.itemName,
+        type: this.type,
+        habitability: this.habitability,
+        oxygen_percent: this.oxygenPercent,
+        discovery_date: this.discovery_date,
+      });
+      this.form_alert = 'Planet Added';
+    },
+  },
+};
 
 </script>
 
 <template>
   <section>
     <h1>Planet Adder</h1>
-    <form class="planet-form">
+    <form class="planet-form" @submit.prevent="addItem">
 
       <div class="planet-form_field">
         <label>Name:</label>
-        <input v-model="name" placeholder="Planet name" />
+        <input v-model="name" placeholder="Planet name" required>
       </div>
 
       <div class="planet-form_field">
-        <label>Oxygen percent: ({{oxygenPercent}}%)</label>
-        <input type="range" v-model="oxygenPercent"
-         min="0" max="100" step="1">
+        <label>Oxygen percent: ({{oxygen_percent}}%)</label>
+        <input type="range" v-model="oxygen_percent"
+         min="0" max="100" step="1" required>
       </div>
 
       <div class="planet-form_field">
-        <label>Habitability: ({{habitability}}%)</label>
-        <input type="range" v-model="habitability"
-         min="0" max="100" step="1">
+        <label>Habitability:</label>
+        <div>
+          <input type="checkbox" v-model="habitability" required>
+        </div>
       </div>
 
       <div class="planet-form_field">
         <label class="label">Type:</label>
-        <select v-model="type">
+        <select v-model="type" required>
           <option v-for="option in allTypes" :value="option.value">
             {{ option.text }}
           </option>
@@ -74,25 +73,20 @@ export default {
       </div>
 
       <div class="planet-form_field">
-        <label class="label">Animals:</label>
-        <select v-model="animals" multiple>
-          <option v-for="option in allAnimals" :value="option.value">
-            {{ option.text }}
-          </option>
-        </select>
+        <label class="label">Distance from Earth:</label>
+        <input type="number" v-model="distance"
+         min="1000" max="100000" step="1000" required>
       </div>
 
       <div class="planet-form_field">
-        <label class="label">Plants:</label>
-        <select v-model="plants" multiple>
-          <option v-for="option in allPlants" :value="option.value">
-            {{ option.text }}
-          </option>
-        </select>
+        <label class="label">Discovery Date:</label>
+        <input type="date" v-model="discovery_date" required>
       </div>
 
       <div class="planet-form_submit">
-        <button type="submit">Add new planet</button>
+        <label class="label">Add new planet:</label>
+        <button>Add new planet</button>
+        <span class="planet-form_alert">{{form_alert}}</span>
       </div>
 
     </form>
