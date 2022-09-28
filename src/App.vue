5331f579d9e6c5fa8ee1e63b96ec3533c0fea9b6
Robert biernat <soulsea.demoncore@gmail.com>
2022-09-28 06:42:45 +0200

    vue3-table-lite

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
 
   <header class="header-main">
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
     <nav>
       <RouterLink to="/">Formularz</RouterLink>
       <RouterLink to="/planet-table">Tabela</RouterLink>
